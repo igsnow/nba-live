93aae16c1bff26cba61bc9ef15cb360cdf7c066e
igsnow <zzy716810918@gmail.com>
2021-12-29 17:36:29 +0800

    点击左侧的treeItem，能够右侧editor编辑区弹出webview页面

--- ScoreTreeProvider.js
+++ ScoreTreeProvider.js
@@ -1,9 +1,11 @@
 const vscode = require('vscode');
+const fs = require('fs')
+const path = require('path')
 const getMatches = require('./src/getNBAInfo').getMatches
 
 exports.myTreeProvider = class MyTreeProvider {
     constructor() {
-
+        this.currentPanel = null
     }
 
     static initMyTreeList() {
@@ -13,7 +15,50 @@ exports.myTreeProvider = class MyTreeProvider {
         // 创建treeItem点击事件, showCollapseAll是否显示右上角的折叠小图标
         const tree = vscode.window.createTreeView('nba_id', { treeDataProvider: myTreeProvider, showCollapseAll: false })
 
-        tree.onDidChangeSelection(e => console.log(e))
+        tree.onDidChangeSelection(e => {
+            console.log('click treeItem ', e);
+
+            const selection = e && e.selection && e.selection[0];
+            const columnToShowIn = vscode.window.activeTextEditor
+                ? vscode.window.activeTextEditor.viewColumn
+                : undefined;
+            this.currentPanel = vscode.window.createWebviewPanel(
+                selection.label[0],
+                selection.label,
+                columnToShowIn,
+                {
+                    // Enable javascript in the webview
+                    enableScripts: true,
+
+                    // Only allow the webview to access resources in our extension's media directory
+                    // localResourceRoots: [this._extensionUri]
+                    // And restrict the webview to only loading content from our extension's `media` directory.
+
+                    // localResourceRoots: [vscode.Uri.file(path.join(this.context.extensionPath, 'src'))]
+                }
+            );
+            this.currentPanel.webview.html = myTreeProvider.generateHtml()
+
+            this.currentPanel.onDidDispose(() => {
+                console.log('onDidDispose')
+            })
+            this.currentPanel.onDidChangeViewState(
+                e => {
+
+                    const panel = e.webviewPanel;
+
+                    panel.title = '我是标题'
+                }
+            );
+        })
+    }
+
+    generateHtml() {
+        //ANCHOR  js文件在root目录的话，fs得访问绝对路径，否则找不到模板文件
+        const templateHtml = fs.readFileSync(path.join(__dirname, '/src/test-webview.html'))
+        let resHtml = templateHtml.toString()
+        console.log('generate html ', resHtml);
+        return resHtml
     }
 
     getTreeItem(el) {
